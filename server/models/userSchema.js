fc46f1ff18feb817e2dd86985c14aa6ec69077e4
dovink <dovydas.in@gmail.com>
2024-03-21 18:46:06 +0200

    Server-auth(registration)

---
+++ server/models/userSchema.js
@@ -0,0 +1,17 @@
+import mangoose from "mongoose";
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mangoose.Schema({
+    name:{type:String, required: [true, "Vardas yra reikalingas"]},
+    email: {type: String, required: [true, "El.pasto adresas yra reikalingas"], unique: true},
+    city: {type: String, required:[ true, "Miestas yra reikalingas"]},
+    password: {type: String, required:[true, "Slaptazodis yra reikalingas"]}
+});
+
+userSchema.pre("save", async function(){
+    this.password = await bcrypt.hash(this.password, 12);
+});
+
+const User = mangoose.model('User', userSchema);
+
+export default User;
---
+++ server/routes/AuthRoute.js
@@ -0,0 +1,9 @@
+import {login, register} from "../Controllers/AuthController.js";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/", register);
+//router.post('/', login);
+
+export default router;
