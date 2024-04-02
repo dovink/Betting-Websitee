b717fe3edcbfb34e7af62839f45bac07817ef6fb
Airis1k <airingasstar@gmail.com>
2024-04-02 23:08:44 +0300

    fixintas config.env failas su email issiuntimu

--- server/util/emailService.js
+++ server/util/emailService.js
@@ -1,25 +1,31 @@
 // emailService.js
 
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    service: process.env.SERVICE,
-    port: process.env.EMAIL_PORT,
-    secure: true,
-    auth: {
+   host: process.env.HOST,
+   service: process.env.SERVICE,
+   port: process.env.EMAIL_PORT,
+   secure: process.env.SECURE,
+   auth: {
       user: process.env.USER,
       pass: process.env.PASS,
-    },
+   },
 });
 
-export const sendConfirmationEmail = async ({ name, email, city, userId, url }) => {
-  try {
-    const mailOptions = {
-      from: email,
-      to: 'projectstatymai@gmail.com',
-      subject: 'Naujo kliento registracija',
-      html: `
+export const sendConfirmationEmail = async ({
+   name,
+   email,
+   city,
+   userId,
+   url,
+}) => {
+   try {
+      const mailOptions = {
+         from: email,
+         to: "projectstatymai@gmail.com",
+         subject: "Naujo kliento registracija",
+         html: `
         <p>Sveiki!,</p>
         <p>Jusu svetaineje nori uzsiregistruoti naujas klientas:</p>
         <ul>
@@ -29,11 +35,11 @@ export const sendConfirmationEmail = async ({ name, email, city, userId, url }) 
         </ul>
         <p><a href="${url}">Spauskite cia kad patvirtinti</a></p>
       `,
-    };
+      };
 
-    await transporter.sendMail(mailOptions);
-    console.log('Confirmation email sent to admin');
-  } catch (error) {
-    console.error('Error sending confirmation email:', error);
-  }
+      await transporter.sendMail(mailOptions);
+      console.log("Confirmation email sent to admin");
+   } catch (error) {
+      console.error("Error sending confirmation email:", error);
+   }
 };
